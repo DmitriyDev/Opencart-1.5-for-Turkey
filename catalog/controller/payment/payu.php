b8efb02f780683ed42f23ec483a4ba0d0c2b5929
Dmitriy <root@PAYU-P-3.devel.entp>
2013-04-04 14:27:11 +0300

    UPD

--- catalog/controller/payment/payu.php
+++ catalog/controller/payment/payu.php
@@ -167,6 +167,20 @@ class PayU
 		return $this;
 	}
 
+
+	function generateHmac($key, $data) {
+        $b = 64; // byte length for md5
+        if (strlen($key) > $b) {
+            $key = pack("H*", md5($key));
+        }
+        $key = str_pad($key, $b, chr(0x00));
+        $ipad = str_pad('', $b, chr(0x36));
+        $opad = str_pad('', $b, chr(0x5c));
+        $k_ipad = $key ^ $ipad;
+        $k_opad = $key ^ $opad;
+        return md5($k_opad . pack("H*", md5($k_ipad . $data)));
+    }
+
 #--------------------------------------------------------
 #	Generate HASH
 #--------------------------------------------------------
@@ -176,7 +190,7 @@ class PayU
 		$this->substr  = $str = "";
 		foreach ( $data as $v ) $str .= $this->convData( $v );
 		echo "\n<br>\n".$this->substr."\n<br>\n".$str."\n<br>\n";
-		return hash_hmac("md5",$str, self::$key);
+		return  $this->generateHmac(self::$key, $str); #hash_hmac("md5",$str, self::$key);
 	}
 
 #--------------------------------------------------------
